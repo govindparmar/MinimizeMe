92108996da97e1a1e4a2a18f3c4252706e31556d
Govind Parmar <govindp@innova-power.com>
2023-01-11 11:27:49 -0700

    Update mainwindow.c
    Fixed bug (using DEFAULT_GUI_FONT when I got the right font from SystemParametersInfoW)

--- mainwindow.c
+++ mainwindow.c
@@ -41,7 +41,7 @@ ATOM RegisterWCEX(_In_ HINSTANCE hInstance)
 
 BOOL CALLBACK EnumChildProc(_In_ HWND hWnd, _In_ LPARAM lParam)
 {
-    HFONT hfDefault = (HFONT) GetStockObject(DEFAULT_GUI_FONT);
+    HFONT hfDefault = *(HFONT *)lParam;
     SendMessageW(hWnd, WM_SETFONT, (WPARAM) hfDefault, 0);
     return TRUE;
 }
